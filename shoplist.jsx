7f0130816a372a6d656170d2b9fff6834f6f30ed
James Donohue <james.brackish@gmail.com>
2016-05-29 11:03:26 +0100

    Filter and sort by closing time

--- shoplist.jsx
+++ shoplist.jsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
 
 import SHOPS from './shops';
 
 const DAYS = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-const today = DAYS[new Date().getDay()];
+const TIME_FORMAT = 'HH:mm';
+
+const now = new Date();
+const today = DAYS[now.getDay()];
+
+const closingHour = (shop, day) => moment(shop.closes[day], TIME_FORMAT).hours();
+const shopsByClosingTime = (shop1, shop2) => closingHour(shop1, today) - closingHour(shop2, today);
 
 class Shop extends React.Component {
     render() {
+        const closes = this.props.data.closes[today];
+
         return (
-            <li>{this.props.data.name}</li>
+            <li>
+                {this.props.data.name}
+                <span> (closes at {closes})</span>
+            </li>
         );
     }
 }
 
 class ShopList extends React.Component {
     render() {
+        const filteredShops = this.props.shops
+            .filter((shop) => shop.closes.hasOwnProperty(today))
+            .sort(shopsByClosingTime);
+
         return (
             <div>
                 <h1>Sushi on {this.props.day}</h1>
                 <ul>
-                    {this.props.shops.map((shop) => <Shop key={shop.name} data={shop}/>)}
+                    {filteredShops.map((shop) => <Shop key={shop.name} data={shop}/>)}
                 </ul>
             </div>
         );
@@ -28,6 +44,6 @@ class ShopList extends React.Component {
 }
 
 ReactDOM.render(
-    <ShopList shops={SHOPS} day={today} />,
+    <ShopList shops={SHOPS} day={today} time={now}/>,
     document.getElementById('container')
 );
