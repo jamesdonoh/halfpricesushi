4e7aef4ff68624cc8f7f4c0eb5f1f6815ef13c5d
James Donohue <james.brackish@gmail.com>
2016-07-08 18:43:31 +0100

    Crude attempt to format relative time without using moment

--- components/Shop.js
+++ components/Shop.js
@@ -5,15 +5,25 @@ import '../styles/shops.css';
 // NB 'stateless function' approach, see
 // https://facebook.github.io/react/docs/reusable-components.html#stateless-functions
 
+const timeToMins = (time) => parseInt(time.substr(0, 2)) * 60 + parseInt(time.substr(3, 2));
+
+const minsUntil = (now, to) => timeToMins(to) - timeToMins(now);
+
+const formatClosing = (currentTime, closes) => {
+    const mins = minsUntil(currentTime, closes);
+    return mins < 30 ? mins + ' min' : closes;
+};
+
 const Shop = (props) =>
     <div className="shop">
         <span className="shop__name">{props.name}</span>
-        <span className="shop__closes">{props.closes}</span>
+        <span className="shop__closes">{formatClosing(props.currentTime, props.closes)}</span>
     </div>
 
 Shop.propTypes = {
     name: React.PropTypes.string.isRequired,
-    closes: React.PropTypes.string.isRequired
+    closes: React.PropTypes.string.isRequired,
+    currentTime: React.PropTypes.string.isRequired
 };
 
 export default Shop;
